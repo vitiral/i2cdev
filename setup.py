400c77a4371c604213800b28bd1ffb65f65ee981
Garrett Berg <gberg@neutral.io>
2015-03-23 13:00:37 -0600

    setup.py: update

--- setup.py
+++ setup.py
@@ -4,10 +4,23 @@ from setuptools import setup
 
 setup(
     name='i2cdev',
-    version='1.2',
+    version='1.2.1',
     description='Simple I2C Library for linux',
     author='Garrett Berg',
     author_email='garrett@cloudformdesign.com',
-    url='git@github.com:cloudformdesign/i2cdev.git',
+    url='https://github.com/cloudformdesign/i2cdev',
     py_modules=['i2cdev'],
+    license='MIT',
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Operating System Kernels',
+        'Topic :: Software Development :: Embedded Systems',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+    ]
 )
